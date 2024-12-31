a25b52e7d68b174bbd115f370e9e76b16b2ba2b0
walae-7 <walaefitout03@gmail.com>
2024-12-31 19:03:35 +0100

    Revert "day 20 finallyyyy"
    This reverts commit c25eb62cce0186d1853af5a67b6da44fd1c16075.

--- AOC_2024/day3/part1_day3.py
+++
@@ -1,8 +0,0 @@
-import re
-list = open("data.txt",'r')
-file = list.read()
-pattern = re.compile(r'mul\(([0-9]{1,3}),([0-9]{1,3})\)')
-sum = 0
-for match in pattern.finditer(file) :
-    sum += (int(match.group(1)))*(int(match.group(2)))
-print(sum)
